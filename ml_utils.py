4728796a8ff3f9fa24c530279e7110fbc8703e80
samara295 <85747122+samara295@users.noreply.github.com>
2021-07-17 20:25:51 +0530

    Update ml_utils.py
    Removed Fit_transform to change the test array shape

--- ml_utils.py
+++ ml_utils.py
@@ -5,6 +5,10 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from imblearn.pipeline import Pipeline
+from imblearn.under_sampling import RepeatedEditedNearestNeighbours
+from sklearn.preprocessing import StandardScaler
 from pandas import read_csv
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
@@ -13,37 +17,34 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import fbeta_score, f1_score,precision_score,recall_score,accuracy_score, roc_curve, auc
 
 
-clf_lr = LogisticRegression(penalty='l2',C=1.0, max_iter=10000)
+#model
+model = LogisticRegression(solver='liblinear', class_weight='balanced')
 
+X, y, cat_ix, num_ix = load_dataset()
+# one hot encode categorical, normalize numerical
+ct = ColumnTransformer([('c',OneHotEncoder(),cat_ix), ('n',MinMaxScaler(),num_ix)])
+# scale, then undersample, then fit model
+pipeline = Pipeline(steps=[('t',ct), ('s', RepeatedEditedNearestNeighbours()), ('m',model)])
+pipeline.fit(X, y)
 
 classes = {0: "Bad risk",1: "Good risk"}
 
-def load_model():
+def load_dataset():
 	df=read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/statlog/german/german.data",sep=" ",header=None)
 	last_ix = len(df.columns) - 1
-	x, y = df.drop(last_ix, axis=1), df[last_ix]
-	# Categorical features has to be converted into integer values for the model to process. 
-	#This is done through one hot encoding.
+	X, y = df.drop(last_ix, axis=1), df[last_ix]
+	
 	# select categorical features
 	cat_ix = x.select_dtypes(include=['object', 'bool']).columns
-	# one hot encode categorical features only
-	ct = ColumnTransformer([('o',OneHotEncoder(),cat_ix)], remainder='passthrough')
-	X = ct.fit_transform(x)
+	num_ix = X.select_dtypes(include=['int64', 'float64']).columns
 	# label encode the target variable to have the classes 0 and 1
 	y = LabelEncoder().fit_transform(y)
+	return X.values, y, cat_ix, num_ix
 	
-	X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2)
-
-	
-	clf_lr.fit(X_train, y_train)
-	accuracy = check_accurary(X,y,X_test,y_test)	
-
-	print(f"accuracy={accuracy}")
-
 def predict(query_data):
 	print(f"query_data.dict().values()={query_data.dict().values()}")
 	x = list(query_data.dict().values())
-	prediction = clf_lr.predict([x])[0] 
+	prediction = pipeline.predict([x])[0] 
 	print(f"prediction={prediction}")
 	print(f"Model prediction: {classes[prediction]}")
 	return classes[prediction]
