fd24ba36b9c8356513e2ba2c6582875b29493591
samara295 <85747122+samara295@users.noreply.github.com>
2021-07-17 20:40:49 +0530

    Update ml_utils.py

--- ml_utils.py
+++ ml_utils.py
@@ -1,5 +1,5 @@
 from os import X_OK
-!pip install imblearn
+!pip install -U imbalanced-learn
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
